f1a58a678b01795d14422859e5d8ebaed71c36c3
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 15:16:03 +0530

    Added Vending Machine Programe

--- Day6LogicalPrograms/Program.cs
+++ Day6LogicalPrograms/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace Day6LogicalPrograms
+public class VendingMachine
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome To Logical Programs");
-        }
-    }
+
+	// function to count and
+	// print currency notes
+	public static void countCurrency(int amount)
+	{
+		int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+		int[] Counter = new int[8];
+
+		// count notes 
+		for (int i = 0; i < 8; i++)
+		{
+			if (amount >= notes[i])
+			{
+				Counter[i] = amount / notes[i];
+				amount = amount - Counter[i] * notes[i];
+			}
+		}
+
+		// Print notes
+		Console.WriteLine("Currency Count ->");
+		for (int i = 0; i < 8; i++)
+		{
+			if (Counter[i] != 0)
+			{
+				Console.WriteLine(notes[i] + " : "
+					+ Counter[i]);
+			}
+		}
+	}
+
+	public static void Main()
+	{
+		Console.WriteLine("Enter Amount");
+		int amount = Convert.ToInt32(Console.ReadLine());
+		countCurrency(amount);
+	}
+
+
 }
