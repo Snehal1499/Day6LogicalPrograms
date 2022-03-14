08c240f6271dd6b0de253332090760729d34e84d
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 14:45:20 +0530

    Added Monthly Pay

--- Day6LogicalPrograms/Program.cs
+++ Day6LogicalPrograms/Program.cs
@@ -2,11 +2,35 @@
 
 namespace Day6LogicalPrograms
 {
-    internal class Program
+    namespace MontlyPay
     {
-        static void Main(string[] args)
+        public class MontlyPayment
         {
-            Console.WriteLine("Welcome To Logical Programs");
+            public static double P, Y, R, r, n, payment, x, y;
+            public static void montlypayment(double P, double Y, double R)
+            {
+                n = 12 * Y;
+                r = R / (12 * 100);
+                x = (1 + r);
+                y = Math.Pow(x, -n);
+                payment = (P * r) / 1 - y;
+
+                Console.WriteLine($"Payment for given values is : {payment}");
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                MontlyPayment mp = new MontlyPayment();
+                Console.WriteLine("Enter P , Y ,R values ");
+                MontlyPayment.P = Convert.ToDouble(Console.ReadLine());
+                MontlyPayment.Y = Convert.ToDouble(Console.ReadLine());
+                MontlyPayment.R = Convert.ToDouble(Console.ReadLine());
+                MontlyPayment.montlypayment(MontlyPayment.P, MontlyPayment.Y, MontlyPayment.R);
+            }
         }
     }
 }
+
+
