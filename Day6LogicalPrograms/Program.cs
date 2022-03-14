e595a41fd3be89a8e9c5b37602345ebbde171fa7
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 15:06:15 +0530

    Added Temperature Conversion

--- Day6LogicalPrograms/Program.cs
+++ Day6LogicalPrograms/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace Day6LogicalPrograms
+namespace TemparatureConversion
 {
+    public class Util
+    {
+        public static double fahrenhiet = 0, celsius = 0;
+        public static void temparatureConversion_F(double fahrenhiet)
+        {
+            Console.WriteLine("Converting Fahrenhiet to Celsius");
+            celsius = (fahrenhiet - 32) * 5 / 9;
+            Console.WriteLine($"Fahrenhiet Temparature : {fahrenhiet} Converted to Celsius :{celsius} ");
+
+        }
+        public static void temparatureConversion_C(double celsius)
+        {
+            Console.WriteLine("Converting Celsius to Fahrenhiet");
+            fahrenhiet = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"Celsius Temparatue : {celsius} Converted to Fahrenhiet : {fahrenhiet}");
+
+        }
+
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Logical Programs");
+
+            Console.WriteLine("Enter Temperature in Fahrenhiet ");
+            Util.fahrenhiet = Convert.ToDouble(Console.ReadLine());
+            Util.temparatureConversion_F(Util.fahrenhiet);
+            Console.WriteLine("Enter Temperature in Celsius");
+            Util.celsius = Convert.ToDouble(Console.ReadLine());
+            Util.temparatureConversion_C(Util.celsius);
         }
     }
 }
