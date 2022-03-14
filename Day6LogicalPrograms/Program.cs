6e726ad2f1e095a3c698465666ae207cacfd724b
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 14:30:55 +0530

    Added Perfect Number

--- Day6LogicalPrograms/Program.cs
+++ Day6LogicalPrograms/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
-namespace Day6LogicalPrograms
+namespace PerfectNumber
 {
+    public class PerfectNumber
+    {
+        public void Perfect(int number)
+        {
+            int sum = 0, n;
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine($"Entered number {number} is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine($"Entered number {number} is not a perfect number");
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Logical Programs");
+            Console.WriteLine("Enter the Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            PerfectNumber p = new PerfectNumber();
+            p.Perfect(number);
+
+
         }
     }
 }
+
+
+
+
