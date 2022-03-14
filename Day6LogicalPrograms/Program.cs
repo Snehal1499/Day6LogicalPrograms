62c920f4d7a57d7edafd44151937833a33448547
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 14:56:08 +0530

    Added Coupon Numbers

--- Day6LogicalPrograms/Program.cs
+++ Day6LogicalPrograms/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace Day6LogicalPrograms
+namespace CoupenNumber
 {
+    public class CoupenNumber
+    {
+        public int N;
+        public static void Coupen(int N)
+        {
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(random.Next());
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Logical Programs");
+            Console.WriteLine("Enter N Value");
+            CoupenNumber c = new CoupenNumber();
+            int N = Convert.ToInt32(Console.ReadLine());
+            CoupenNumber.Coupen(N);
+
         }
     }
 }
