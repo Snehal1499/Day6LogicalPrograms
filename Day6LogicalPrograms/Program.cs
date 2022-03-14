0b87ff24018c399712e218665138f805d8153d26
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 14:38:01 +0530

    Added Reverse Number

--- Day6LogicalPrograms/Program.cs
+++ Day6LogicalPrograms/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace Day6LogicalPrograms
+public class ReverseNumber
 {
-    internal class Program
+    int reverse = 0, rem;
+    public void Reverse_number(int number)
     {
-        static void Main(string[] args)
+
+        while (number != 0)
         {
-            Console.WriteLine("Welcome To Logical Programs");
+            rem = number % 10;
+            reverse = reverse * 10 + rem;
+            number /= 10;
         }
+        Console.Write("Reversed Number: " + reverse);
+    }
+}
+public class Reverse
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        ReverseNumber r = new ReverseNumber();
+        r.Reverse_number(number);
+
     }
 }
