db123987782a77407383403ed54df6e51668cf2b
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 15:23:23 +0530

    Added Binary Swap Number

--- Day6LogicalPrograms/Program.cs
+++ Day6LogicalPrograms/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace Day6LogicalPrograms
+namespace Binary
 {
-    internal class Program
+    public class Binary
     {
-        static void Main(string[] args)
+        public static string answer, result;
+        public static int num;
+        public static void toBinary(int num)
         {
-            Console.WriteLine("Welcome To Logical Programs");
+            result = "";
+            while (num > 1)
+            {
+                int remainder = num % 2;
+                Binary.result = Convert.ToString(remainder) + Binary.result;
+                num /= 2;
+            }
+            Binary.result = Convert.ToString(num) + Binary.result;
+            Console.WriteLine($"Binary: {Binary.result}");
+
+        }
+        public void Swap(int num)
+        {
+            Console.WriteLine(Convert.ToString(((Binary.num & 0x0F) << 4) | ((Binary.num & 0xF0) >> 4)));
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Binary b = new Binary();
+            Console.Write("Input a Number : ");
+            Binary.num = Convert.ToInt32(Console.ReadLine());
+            Binary.toBinary(Binary.num);
+            b.Swap(Binary.num);
+
         }
     }
 }
